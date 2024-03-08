539a675277c27bc7ef9fc834fcc4c4f385f0e0f5
Nikita Salunke <150005212+nikitasidramsalunke@users.noreply.github.com>
2024-03-09 00:01:06 +0530

    Add files via upload

---
+++ DaysOfWeelSwitch.java
@@ -0,0 +1,29 @@
+class DaysOfWeelSwitch
+{
+public static void main(String[]args)
+{
+int daynum=4;
+switch (daynum)
+{
+case 1:System.out.println("monday");
+       break;
+case 2:System.out.println("tuesday");
+       break;
+case 3:System.out.println("wednesday");
+       break;
+case 4:System.out.println("thursday");
+        break;
+case 5:System.out.println("friday");
+       break;
+case 6:System.out.println("saturday");
+       break;
+case 7:System.out.println("sunday");
+       break;
+
+ default: System.out.println("not valid");
+          break;
+
+}
+
+}
+}
